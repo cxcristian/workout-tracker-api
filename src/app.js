aa0ab369f63f84a6fbfb5bf6546be27d2c92658b
Cristiandavid3d@gmail.com <Cristian>
2025-09-13 10:41:26 -0500

    configurar el servidor de express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 console.log("workout-tracker app");
+
+const express = require("express")//importacion de express, commonjs
+const app = express()//creamos una instancia de express, donde app tiene todo de express
+const port = 8000 //puerto de escucha
+//Inicializacion del servidor
+
+app.get("/", (req, res)=>{
+    res.send("Hola mi servidor de express")
+})
+
+app.listen(port, ()=>{
+    console.log(`Servidor corriendo en http://localhost:${port}`)
+})
